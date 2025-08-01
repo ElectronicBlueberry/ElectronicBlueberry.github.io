4d794130fada80521953934a639dd386e194125a
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2025-08-01 22:36:29 +0200

    add css reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/image"],
+	css: ["~/assets/css/main.css"],
 	postcss: {
 		plugins: {
 			"postcss-nested": {},
