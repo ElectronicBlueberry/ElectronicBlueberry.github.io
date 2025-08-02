3fcf5d9e98d76f81fb2edba932061b01fd072947
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2025-08-02 22:45:49 +0200

    add title card

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,5 +5,38 @@ const { data: page } = await useAsyncData("main", () => {
 </script>
 
 <template>
-	<ContentRenderer v-if="page" :value="page" />
+	<div class="index-page">
+		<div class="title-card">
+			<h1>Laila Los</h1>
+			<span> Front-End Developer </span>
+		</div>
+
+		<ContentRenderer v-if="page" :value="page" />
+	</div>
 </template>
+
+<style lang="postcss" scoped>
+.index-page {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.title-card {
+	background-color: var(--color-purple-200);
+	padding: var(--spacing-2) var(--spacing-8);
+	margin: var(--spacing-8) 0;
+	border-radius: 999px;
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 400;
+	}
+
+	span {
+		font-size: 1.5rem;
+		line-height: 1;
+		font-weight: 100;
+	}
+}
+</style>
